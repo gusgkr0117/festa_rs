9f8e7d4721f86dec72cb1c453b6724c2cccf70a6
khh <khh@mail.com>
2024-02-05 21:54:16 -0800

    update festa

--- src/festa.rs
+++ src/festa.rs
@@ -1,16 +1,16 @@
 //! keygen, encrypt and decrypt of FESTA Public key encryption scheme
 //! It's an implementation of FESTA-128
 use crate::{
-    fields::{
+    ecFESTA::factored_kummer_isogeny, fields::{
         FpFESTA::Fp,
-        FpFESTAExt::{P0x, P0y, Q0x, Q0y, BASIS_ORDER, DA, DA1, DA2},
-    },
-    thetaFESTA::{
+        FpFESTAExt::{P0x, P0y, Q0x, Q0y, BASIS_ORDER, D1, DA, DA1, DA1_FACTORED, DA2, DA2_FACTORED, DA_FACTORED, L_POWER},
+    }, supersingular::{random_isogeny_x_only, torsion_basis}, thetaFESTA::{
         product_isogeny, CouplePoint, Curve, EllipticProduct, Fq, KummerLineIsogeny, Point,
-    },
+    }
 };
 
 use num_bigint::{BigUint, RandBigInt};
+use rand::random;
 
 /// Publickey structure of FESTA
 pub struct PublicKey {
@@ -21,9 +21,11 @@ pub struct PublicKey {
 
 /// Secretkey structure of FESTA
 pub struct SecretKey {
-    phiA: KummerLineIsogeny,
+    phiA: Vec<KummerLineIsogeny>,
     diagmatA: (BigUint, BigUint),
 }
+
+
 
 /// Generate a pair of public key and secret key of FESTA PKE
 pub fn keygen() {
@@ -34,19 +36,28 @@ pub fn keygen() {
     // Choose a random diagonal matrix A
     let (a11, a22) = (rng.gen_biguint(256), rng.gen_biguint(256));
 
-    // Choose a random basis of order dA
-    let basis_P0 = Point::new_xy(&Fq::new(&P0x, &Fp::ZERO), &Fq::new(&P0y, &Fp::ZERO));
-    let basis_Q0 = Point::new_xy(&Fq::new(&Q0x, &Fp::ZERO), &Fq::new(&Q0y, &Fp::ZERO));
+    // Choose a random isogeny of degree dA1
+    let (phi1, EA1) = random_isogeny_x_only(&E0, &DA1_FACTORED, 2);
+
+    // Choose a random isogeny of degree dA2
+    let (phi2, EA2) = random_isogeny_x_only(&EA1, &DA2_FACTORED, 2);
+
+    // Create a 2^b-torsion basis
+    let l_power = BigUint::from(2u32).pow(L_POWER);
+    let d1 = BigUint::from_slice(&D1);
+    let (Pb, Qb) = torsion_basis(&E0, &l_power, 0);
+    let (Pd1, Qd1) = torsion_basis(&E0, &d1, L_POWER as usize);
+
+    let (Pd1b, Qd1b) = (E0.add(&Pb, &Pd1), E0.add(&Qb, &Qd1));
     
-    let da = BigUint::from_slice(&DA);
-    let basis_order = BigUint::from_slice(&BASIS_ORDER);
-    let cofactor_da = basis_order/da;
-    let basis_PA = E0.mul_big(&basis_P0, &cofactor_da);
-    let mut basis_QA = E0.mul_big(&basis_Q0, &cofactor_da);
+}
 
-    let random_secret = rng.gen_biguint(256);
-    basis_QA = E0.mul_big(&basis_QA, &random_secret);
+#[cfg(test)]
+mod tests{
+    use super::keygen;
 
-    // Compute the kernel of order dA1
-    let da1_kernel = E0.mul_big(&basis_QA, &BigUint::from_slice(&DA2));
+    #[test]
+    fn run_keygen() {
+        keygen();
+    }
 }
