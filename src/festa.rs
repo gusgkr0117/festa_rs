ba68f1be416ba710226150e0930f035875d6640d
gusgkr0117 <gusgkr0117@naver.com>
2024-02-06 22:18:22 +0900

    update the use of the torsion basis function in keygen

--- src/festa.rs
+++ src/festa.rs
@@ -1,12 +1,18 @@
 //! keygen, encrypt and decrypt of FESTA Public key encryption scheme
 //! It's an implementation of FESTA-128
 use crate::{
-    ecFESTA::factored_kummer_isogeny, fields::{
+    ecFESTA::factored_kummer_isogeny,
+    fields::{
         FpFESTA::Fp,
-        FpFESTAExt::{P0x, P0y, Q0x, Q0y, BASIS_ORDER, D1, DA, DA1, DA1_FACTORED, DA2, DA2_FACTORED, DA_FACTORED, L_POWER},
-    }, supersingular::{random_isogeny_x_only, torsion_basis}, thetaFESTA::{
+        FpFESTAExt::{
+            P0x, P0y, Q0x, Q0y, BASIS_ORDER, D1, D1_FACTORED, DA, DA1, DA1_FACTORED, DA2,
+            DA2_FACTORED, DA_FACTORED, L_POWER,
+        },
+    },
+    supersingular::{random_isogeny_x_only, torsion_basis},
+    thetaFESTA::{
         product_isogeny, CouplePoint, Curve, EllipticProduct, Fq, KummerLineIsogeny, Point,
-    }
+    },
 };
 
 use num_bigint::{BigUint, RandBigInt};
@@ -24,8 +30,6 @@ pub struct SecretKey {
     phiA: Vec<KummerLineIsogeny>,
     diagmatA: (BigUint, BigUint),
 }
-
-
 
 /// Generate a pair of public key and secret key of FESTA PKE
 pub fn keygen() {
@@ -45,15 +49,14 @@ pub fn keygen() {
     // Create a 2^b-torsion basis
     let l_power = BigUint::from(2u32).pow(L_POWER);
     let d1 = BigUint::from_slice(&D1);
-    let (Pb, Qb) = torsion_basis(&E0, &l_power, 0);
-    let (Pd1, Qd1) = torsion_basis(&E0, &d1, L_POWER as usize);
+    let (Pb, Qb) = torsion_basis(&E0, &[(2u32, L_POWER)], 0);
+    let (Pd1, Qd1) = torsion_basis(&E0, &D1_FACTORED, L_POWER as usize);
 
     let (Pd1b, Qd1b) = (E0.add(&Pb, &Pd1), E0.add(&Qb, &Qd1));
-    
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::keygen;
 
     #[test]
