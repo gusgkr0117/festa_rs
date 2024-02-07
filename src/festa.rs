ce5ee185842283236d42ed06a7ee27dd70955b30
khh <khh@mail.com>
2024-02-07 00:27:17 -0800

    update keygen

--- src/festa.rs
+++ src/festa.rs
@@ -1,12 +1,12 @@
 //! keygen, encrypt and decrypt of FESTA Public key encryption scheme
 //! It's an implementation of FESTA-128
 use crate::{
-    ecFESTA::factored_kummer_isogeny,
+    ecFESTA::{evaluate_isogeny_chain, factored_kummer_isogeny},
     fields::{
         FpFESTA::Fp,
         FpFESTAExt::{
-            P0x, P0y, Q0x, Q0y, BASIS_ORDER, D1, D1_FACTORED, DA, DA1, DA1_FACTORED, DA2,
-            DA2_FACTORED, DA_FACTORED, L_POWER,
+            P0x, P0y, Q0x, Q0y, BASIS_ORDER, D1, D1_FACTORED, D2_FACTORED, DA, DA1, DA1_FACTORED,
+            DA2, DA2_FACTORED, DA_FACTORED, L_POWER,
         },
     },
     supersingular::{random_isogeny_x_only, torsion_basis},
@@ -25,10 +25,38 @@ pub struct PublicKey {
     SA: Point,
 }
 
+impl PublicKey {
+    pub fn new(EA: &Curve, RA: &Point, SA: &Point) -> Self {
+        PublicKey {
+            EA: *EA,
+            RA: *RA,
+            SA: *SA,
+        }
+    }
+}
+
 /// Secretkey structure of FESTA
 pub struct SecretKey {
-    phiA: Vec<KummerLineIsogeny>,
+    EA_prime: Curve,
+    im_basis_bd1_E0: (Point, Point),
+    im_basis_d2_EA: (Point, Point),
     diagmatA: (BigUint, BigUint),
+}
+
+impl SecretKey {
+    pub fn new(
+        EA_prime: &Curve,
+        im_basis_bd1_E0: &(Point, Point),
+        im_basis_d2_EA: &(Point, Point),
+        diagmatA: &(BigUint, BigUint),
+    ) -> Self {
+        SecretKey {
+            EA_prime: *EA_prime,
+            im_basis_bd1_E0: *im_basis_bd1_E0,
+            im_basis_d2_EA: *im_basis_d2_EA,
+            diagmatA: diagmatA.clone(),
+        }
+    }
 }
 
 /// Generate a pair of public key and secret key of FESTA PKE
@@ -41,18 +69,42 @@ pub fn keygen() {
     let (a11, a22) = (rng.gen_biguint(256), rng.gen_biguint(256));
 
     // Choose a random isogeny of degree dA1
-    let (phi1, EA1) = random_isogeny_x_only(&E0, &DA1_FACTORED, 2);
-
+    let (phiA1, EA_prime) = random_isogeny_x_only(&E0, &DA1_FACTORED, 2);
     // Choose a random isogeny of degree dA2
-    let (phi2, EA2) = random_isogeny_x_only(&EA1, &DA2_FACTORED, 2);
+    let (phiA2, EA) = random_isogeny_x_only(&EA_prime, &DA2_FACTORED, 2);
 
     // Create a 2^b-torsion basis
     let l_power = BigUint::from(2u32).pow(L_POWER);
     let d1 = BigUint::from_slice(&D1);
     let (Pb, Qb) = torsion_basis(&E0, &[(2u32, L_POWER)], 0);
     let (Pd1, Qd1) = torsion_basis(&E0, &D1_FACTORED, L_POWER as usize);
+    let (PA_prime_d2, QA_prime_d2) = torsion_basis(&EA_prime, &D2_FACTORED, L_POWER as usize);
 
     let (Pd1b, Qd1b) = (E0.add(&Pb, &Pd1), E0.add(&Qb, &Qd1));
+    let ((_, imPd1b), (_, imQd1b)) = (
+        evaluate_isogeny_chain(&E0, &Pd1b, &phiA1),
+        evaluate_isogeny_chain(&E0, &Qd1b, &phiA1),
+    );
+
+    let (imPb, imQb) = (
+        EA_prime.mul_big(&imPd1b, &d1),
+        EA_prime.mul_big(&imQd1b, &d1),
+    );
+
+    let (Pbd2, Qbd2) = (
+        EA_prime.add(&imPb, &PA_prime_d2),
+        EA_prime.add(&imQb, &QA_prime_d2),
+    );
+
+    let ((_, imPbd2), (_, imQbd2)) = (
+        evaluate_isogeny_chain(&EA_prime, &Pbd2, &phiA2),
+        evaluate_isogeny_chain(&EA_prime, &Qbd2, &phiA2),
+    );
+
+    let (imPAd2, imQAd2) = (
+        EA_prime.mul_big(&imPbd2, &l_power),
+        EA_prime.mul_big(&imQbd2, &l_power),
+    );
 }
 
 #[cfg(test)]
